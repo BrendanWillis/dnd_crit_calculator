d6de47b5ffc9b2e1aac6f216cfaa06dd31b0d097
vuulgen <97462952+vuulgen@users.noreply.github.com>
2025-06-03 17:34:50 -0600

    Initialized Git and created project files

---
+++ target/debug/deps/dnd_crit_calculator-23d417ad96b7c000.d
@@ -0,0 +1,5 @@
+/Users/brendanwillis/dnd_crit_calculator/target/debug/deps/dnd_crit_calculator-23d417ad96b7c000: src/main.rs
+
+/Users/brendanwillis/dnd_crit_calculator/target/debug/deps/dnd_crit_calculator-23d417ad96b7c000.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/dnd_crit_calculator.d
@@ -0,0 +1,1 @@
+/Users/brendanwillis/dnd_crit_calculator/target/debug/dnd_crit_calculator: /Users/brendanwillis/dnd_crit_calculator/src/main.rs
